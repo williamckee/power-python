b32b1162bc82df364204e229961d01ffeb29fc27
William <keewilliam@protonmail.com>
2022-12-08 15:21:31 -0500

    -changed the ui -combo list has preset items -added a reload button -added a window to display action items

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import subprocess
 import sys
 import PySimpleGUI as sg
 
-# Window Color Pallet
 sg.theme('Dark Blue 3')
-layout = [[sg.Text('Export users in AD:'), sg.Button('Export')],
-          [sg.Text('Export Office 365 Mailboxes:'), sg.Button('Run')],
-          [sg.Text('Export Office 365 OneDrive data:'), sg.Button('Run')],
-          [sg.Output(size=(60, 20))],  # Progress output for commands
-          [sg.Button('Close')]
-          ]
+layout = [
+    [sg.Combo(['Export AD Users', 'Export O365 Mailbox Data', 'Export OneDrive Data'], size=(60, 20),
+              enable_events=True,key='selected')],
+    [sg.Output(size=(60, 20))],  # Progress output for commands
+    [sg.Button('Reload'),sg.Button('Run'), sg.Button('Close')]
+]
 
 # Create the Window
 window = sg.Window('Power Python', layout)
@@ -17,16 +16,14 @@ window = sg.Window('Power Python', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Close':  # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Close':
         break
-    print('You entered ', values[0])
 
-    if event == 'Export':
+    if event == 'Reload':
+        print('Getting scripts...')
+        window.refresh()
 
-        break
+    if event == 'Run':
+        item = values['selected']
+        print(item + '...')
 window.close()
-
-# PowerShell subprocesses
-expAD = subprocess.check_output("")
-exptMB = subprocess.check_output("")
-expOD = subprocess.check_output("")
